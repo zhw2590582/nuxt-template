a4d84d135d654a641a85cec333c1b6fb591ef4ee
Harvey Zhao <laozhaochaguan@gmail.com>
2026-01-20 11:06:30 +0800

    feat: add initial configuration and environment setup
    - Created .env file with API secret token
    - Added app configuration in app.config.ts with title and theme settings
    - Initialized tailwind.config.js for styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+    runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123',
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: '/api',
+    },
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/content', '@nuxt/eslint']
 })
