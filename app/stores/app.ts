bf2eae8b3ab228fab73b9284ce54aa944d4c7fcd
Harvey Zhao <laozhaochaguan@gmail.com>
2026-01-20 14:40:49 +0800

    refactor: update app and counter stores to use state, getters, and actions syntax

--- app/stores/app.ts
+++ app/stores/app.ts
@@ -1,13 +1,13 @@
-export const useAppStore = defineStore('app', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
+export const useAppStore = defineStore('app', {
+  state: () => ({
+    count: 0,
+  }),
+  getters: {
+    doubleCount: state => state.count * 2,
+  },
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
 })
-
-if (import.meta.hot) {
-  import.meta.hot.accept(acceptHMRUpdate(useAppStore, import.meta.hot))
-}
--- app/stores/counter.ts
+++ app/stores/counter.ts
@@ -1,13 +1,13 @@
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
+export const useCounterStore = defineStore('counter', {
+  state: () => ({
+    count: 0,
+  }),
+  getters: {
+    doubleCount: state => state.count * 2,
+  },
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
 })
-
-if (import.meta.hot) {
-  import.meta.hot.accept(acceptHMRUpdate(useCounterStore, import.meta.hot))
-}
